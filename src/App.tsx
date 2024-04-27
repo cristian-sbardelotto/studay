b6cbf1ee36b4b32256a0e3d7457292435ed9d56e
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-04-27 19:09:38 -0300

    initial commit: set up shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button';
+
+export function App() {
+  return (
+    <>
+      <Button variant='default'>Hello World</Button>
+      <Button variant='destructive'>Hello World</Button>
+      <Button variant='ghost'>Hello World</Button>
+      <Button variant='link'>Hello World</Button>
+      <Button variant='outline'>Hello World</Button>
+      <Button variant='secondary'>Hello World</Button>
+    </>
+  );
+}
