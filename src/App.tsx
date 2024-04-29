2c58f66c3dd36e1802701245454440e445809d04
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-04-29 06:59:00 -0300

    feat: add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import { Header } from './components/header';
+import { Footer } from './components/footer';
 
 export function App() {
-  return <Header />;
+  return (
+    <div className='h-full flex flex-col'>
+      <Header />
+
+      <main className='flex-1'>
+        <h2>Home</h2>
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,81 @@
+import { Button } from './ui/button';
+
+import { GithubIcon, LinkedinIcon, MailIcon } from 'lucide-react';
+
+export function Footer() {
+  return (
+    <footer className='border-t border-muted'>
+      <div className='container px-4 py-7 flex items-center justify-between max-sm:flex-col max-sm:gap-3'>
+        <p className='text-muted-foreground'>
+          Made with 🧡 by{' '}
+          <Button
+            className='p-0 m-0 text-base'
+            asChild
+            variant='link'
+          >
+            <a
+              href='https://linkedin.com/in/cristian-k-sbardelotto/'
+              target='_blank'
+              className='text-primary'
+            >
+              Cristian Sbardelotto
+            </a>
+          </Button>
+          .
+        </p>
+
+        <nav>
+          <ul className='flex h-6 items-center gap-3 text-muted-foreground'>
+            <li>
+              <Button
+                asChild
+                variant='outline'
+                size='icon'
+                className='py-0.5 px-2'
+              >
+                <a
+                  href='https://github.com/cristian-sbardelotto'
+                  target='_blank'
+                >
+                  <GithubIcon size={18} />
+                </a>
+              </Button>
+            </li>
+
+            <li>
+              <Button
+                asChild
+                variant='outline'
+                size='icon'
+                className='py-0.5 px-2'
+              >
+                <a
+                  href='https://linkedin.com/in/cristian-k-sbardelotto'
+                  target='_blank'
+                >
+                  <LinkedinIcon size={18} />
+                </a>
+              </Button>
+            </li>
+
+            <li>
+              <Button
+                asChild
+                variant='outline'
+                size='icon'
+                className='py-0.5 px-2'
+              >
+                <a
+                  href='https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=cristian.k.sbardelotto@gmail.com'
+                  target='_blank'
+                >
+                  <MailIcon size={18} />
+                </a>
+              </Button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+}
