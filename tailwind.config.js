12e8d296cc9b3ce7594db071a4d5fb8f7465ad1c
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-04-29 07:54:04 -0300

    fix: remove warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   darkMode: ['class'],
   content: [
-    './*{html}',
+    './*html',
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
