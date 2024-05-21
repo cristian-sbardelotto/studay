7b3b09b5071659174f54cb5ed421d51a7707b78f
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-05-21 14:29:54 -0300

    feat: add Homework type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,15 @@
 import { z } from 'zod';
 import { createHomeWorkFormSchema } from './lib/zod/schemas';
 
+export type Homework = {
+  id: string;
+  title: string;
+  description: string;
+  subject: string;
+  deadline: Date;
+  done: boolean;
+  priority: 'low' | 'medium' | 'high';
+  links?: string[];
+};
+
 export type FormFields = z.infer<typeof createHomeWorkFormSchema>;
