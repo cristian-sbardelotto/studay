30ab857fdf3e383af96eba599b91d71138d19af3
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-05-21 14:29:21 -0300

    refactor: add homeworks mock file

---
+++ src/data/homeworks-mock.ts
@@ -0,0 +1,35 @@
+import { Homework } from '@/types';
+
+import { v4 as randomUUID } from 'uuid';
+
+export const homeworksMock: Homework[] = [
+  {
+    id: randomUUID(),
+    title: 'Prova',
+    description: 'Prova relacionada ao conteúdo de variações de temperatura',
+    priority: 'high',
+    subject: 'Física',
+    deadline: new Date(),
+    done: false,
+  },
+  {
+    id: randomUUID(),
+    title: 'Trabalho',
+    description:
+      'Trabalho de pesquisa em dupla sobre o teorema de Pitágoras e sua criação',
+    priority: 'medium',
+    subject: 'Matemática',
+    deadline: new Date(),
+    done: false,
+  },
+  {
+    id: randomUUID(),
+    title: 'Desenho',
+    description:
+      'Desenho que envolva conceitos como surrealismo, trabalhados em aula',
+    priority: 'low',
+    subject: 'Física',
+    deadline: new Date(),
+    done: false,
+  },
+];
