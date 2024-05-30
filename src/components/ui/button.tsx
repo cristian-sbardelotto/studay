fbeab1c7b957d49f4e2839a90aefbf94891c2601
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2024-05-30 11:34:13 -0300

    feat: add new Button variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,6 +11,7 @@ const buttonVariants = cva(
       variant: {
         default:
           'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+        success: 'bg-green-800 text-primary shadow hover:bg-green-800/90',
         destructive:
           'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
